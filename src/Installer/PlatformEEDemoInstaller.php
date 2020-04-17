e528edccc1f0f2d99bb0450307560a052989e9a5
ciastektk <tomasz.kryszan@ez.no>
2020-04-17 16:50:35 +0200

    DEMO-332: Implemented homepage (#179)
    Added landing page fieldtype for content type Home

--- src/Installer/PlatformEEDemoInstaller.php
+++ src/Installer/PlatformEEDemoInstaller.php
@@ -50,6 +50,7 @@ class PlatformEEDemoInstaller extends CoreInstaller
             'kaliop:migration:migrate --path=src/MigrationVersions/content.yaml -v -n',
             'kaliop:migration:migrate --path=src/MigrationVersions/professionals.yaml -v -n',
             'kaliop:migration:migrate --path=src/MigrationVersions/landing_page.yaml -v -n',
+            'kaliop:migration:migrate --path=src/MigrationVersions/home.yaml -v -n',
 //            'kaliop:migration:migrate --path=src/MigrationVersions/users.yaml -v -n',
         ];
 
